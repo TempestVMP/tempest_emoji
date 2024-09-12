1950e3ffb5d5a7fc6ffeefd8e7bbf95b6821568e
TaerAttO <surapun.khongthong@gmail.com>
2024-09-12 18:04:16 +0700

    description th & add some emoji

--- config.lua
+++ config.lua
@@ -1,15 +1,17 @@
 Cfg = {
-    openMenuKey = 'F5', -- Emoji Menu Key
-    duration = 5000, -- duration displaying emoji
-    distance = 10.0, -- radius can see emoji
+    openMenuKey = 'F5', -- Emoji Menu Key -- TH ปุ่มสำหรับ เปิดเมนู Emoji
+    duration = 5000, -- duration displaying emoji -- TH ระยะเวลาที่แสดง emoji
+    distance = 10.0, -- radius can see emoji -- TH รัศมีที่สามารถมองเห็นได้
     emoji = {
         -- Emoji: https://www.flaticon.com/free-icons/emoji
+        -- สำหรับไปหา Emoji url มาเพิ่มในหมวด ค่าเริ่มต้น
         default = {
             'https://cdn-icons-png.flaticon.com/512/742/742751.png',
             'https://cdn-icons-png.flaticon.com/512/2584/2584602.png',
             'https://cdn-icons-png.flaticon.com/512/743/743418.png',
             'https://cdn-icons-png.flaticon.com/512/4160/4160766.png',
-            
+            'https://cdn-icons-png.flaticon.com/512/4160/4160718.png',
+            'https://cdn-icons-png.flaticon.com/512/743/743255.png',
         },
     },
 }
--- web/config.js
+++ web/config.js
@@ -1,8 +1,10 @@
 cfg = {}
 
 // This for test when you [open with live server] extension Name: Live Server https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
+// ส่วนนี้ไว้สำหรับทดสอบ ถ้าคุณใช้ live server https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
 cfg.debugData = {
     // Emoji: https://www.flaticon.com/free-icons/emoji
+    // สำหรับ เอาลิ้งค์ emoji มาเพิ่มหากต้องการทดสอบเพิ่ม
     default: [
         'https://cdn-icons-png.flaticon.com/512/742/742751.png',
         'https://cdn-icons-png.flaticon.com/512/2584/2584602.png',
